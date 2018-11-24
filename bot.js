4ec2cdd25546f7452f13adc7310d573b1d320303
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-11-24 17:59:14 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require(`discord.js`);
+const client = new Discord.Client();
+
+
+
+client.on('ready' , () => {
+client.user.setGame(`ا̍نۨــٰ̍ا̍ بــحۡــبــگ `,'https://www.twitch.tv/marygaming_')
+console.log(`لا تنسا الاششتراك `)
+
+});
+
+
+client.login("MzQ4OTUzMTQwMzE1MjkxNjQ5.Dp998A.6tT2KKXRZ3EItpO9DRafk1g56Qc")
